ac8c5e71a838a8fce47a71b4d0d19361c407b97a
NaofumiMitani <76736044+NaofumiMitani@users.noreply.github.com>
2021-06-20 14:50:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 /* global $ MobileDetect */
 
-//top画像切り替え
-const imgs = $('.img');
-let Img = -1;
-imgs.hide()
 
-function showNextImg() {
-    Img++;
-    imgs.eq(Img % imgs.length).fadeIn(1500).delay(10000).fadeOut(1500, showNextImg);
-  }
-  showNextImg();
-
+$(function(){
+    $('.Toggle').click(function() {
+    $(this).toggleClass('active');
+    $('.nav-links').toggleClass('open');
+    });
+});
